82be1da0bcde07cb43796e585507232da59ac8e6
nambalaraghava-123 <66437443+nambalaraghava-123@users.noreply.github.com>
2020-06-05 10:54:42 +0530

    Add files via upload

---
+++ secondassign.py
@@ -0,0 +1,22 @@
+"""assigning elements to different lists"""
+l1=[1,2,3,4,5]
+l2=[2,4,2,5,3]
+l3[0]="1"
+l3[1]="2"
+l3[2]="33"
+"""accessing elements from a tuple"""
+t=(1,2,3,4,5)
+t[1:-1]
+"""Deleting different dictionary elements"""
+my=dict()
+mine=dict()
+my[1]="raghava"
+my[2]="hardwork"
+my[3]="software"
+mine[1]="doing work"
+mine[2]="happy"
+mine[3]="work"
+
+"""deletion of different dictionaries"""
+my.pop(1)
+mine.pop(1)
